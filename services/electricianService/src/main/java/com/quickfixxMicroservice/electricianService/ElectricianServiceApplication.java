7ffb52d6d35a6fbdca3fb30b1d8299bf74bccef8
Harsh Rajendra Saindane <harshsaindane1711@gmail.com>
2023-10-09 00:51:06 +0530

    First commit-Adding files Added service- Electrician

---
+++ services/electricianService/src/main/java/com/quickfixxMicroservice/electricianService/ElectricianServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quickfixxMicroservice.electricianService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricianServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricianServiceApplication.class, args);
+	}
+
+}
